0c57b37d8021287bdbcc9513650ae6695993675b
Le Thi Dao <130704lethidao@gmail.com>
2025-03-11 09:36:15 +0700

    giao dien khoa hoc

--- HocTiengAnh/Program.cs
+++ HocTiengAnh/Program.cs
@@ -16,7 +16,7 @@ namespace HocTiengAnh
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TrangChu());
+            Application.Run(new DsKhoaHoc());
         }
     }
 }
