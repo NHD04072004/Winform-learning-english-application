2104ea795553be83783d7b7cc5b4564f20c803cf
NguyenQuocToan0605 <ngquoctoan662005@gmail.com>
2025-03-18 16:51:12 +0700

    Danh sach kết quả

---
+++ HocTiengAnh/DanhSachKetQua.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace HocTiengAnh
+{
+    partial class DanhSachKetQua
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnKhoaHoc = new System.Windows.Forms.Button();
+            this.btnHiennguoidung = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBaihoc4 = new System.Windows.Forms.Button();
+            this.btnBaihoc3 = new System.Windows.Forms.Button();
+            this.btnBaihoc2 = new System.Windows.Forms.Button();
+            this.btnBaihoc1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dgvKetquabailam = new System.Windows.Forms.DataGridView();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKetquabailam)).BeginInit();
+            this.panel10.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.LightGray;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.btnKhoaHoc);
+            this.panel3.Controls.Add(this.btnHiennguoidung);
+            this.panel3.Location = new System.Drawing.Point(0, 1);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(373, 1057);
+            this.panel3.TabIndex = 1;
+            // 
+            // btnKhoaHoc
+            // 
+            this.btnKhoaHoc.BackColor = System.Drawing.Color.Silver;
+            this.btnKhoaHoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKhoaHoc.Location = new System.Drawing.Point(70, 216);
+            this.btnKhoaHoc.Name = "btnKhoaHoc";
+            this.btnKhoaHoc.Size = new System.Drawing.Size(232, 66);
+            this.btnKhoaHoc.TabIndex = 5;
+            this.btnKhoaHoc.Text = "KHÓA HỌC";
+            this.btnKhoaHoc.UseVisualStyleBackColor = false;
+            // 
+            // btnHiennguoidung
+            // 
+            this.btnHiennguoidung.BackColor = System.Drawing.Color.Silver;
+            this.btnHiennguoidung.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHiennguoidung.Image = global::HocTiengAnh.Properties.Resources.user;
+            this.btnHiennguoidung.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHiennguoidung.Location = new System.Drawing.Point(70, 931);
+            this.btnHiennguoidung.Name = "btnHiennguoidung";
+            this.btnHiennguoidung.Size = new System.Drawing.Size(232, 66);
+            this.btnHiennguoidung.TabIndex = 4;
+            this.btnHiennguoidung.UseVisualStyleBackColor = false;
+            this.btnHiennguoidung.Click += new System.EventHandler(this.btnHiennguoidung_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnBaihoc4);
+            this.panel1.Controls.Add(this.btnBaihoc3);
+            this.panel1.Controls.Add(this.btnBaihoc2);
+            this.panel1.Controls.Add(this.btnBaihoc1);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Location = new System.Drawing.Point(509, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1404, 320);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnBaihoc4
+            // 
+            this.btnBaihoc4.BackColor = System.Drawing.Color.Silver;
+            this.btnBaihoc4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBaihoc4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBaihoc4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBaihoc4.Location = new System.Drawing.Point(1078, 114);
+            this.btnBaihoc4.Name = "btnBaihoc4";
+            this.btnBaihoc4.Size = new System.Drawing.Size(260, 180);
+            this.btnBaihoc4.TabIndex = 6;
+            this.btnBaihoc4.UseVisualStyleBackColor = false;
+            this.btnBaihoc4.Click += new System.EventHandler(this.btnBaihoc4_Click);
+            // 
+            // btnBaihoc3
+            // 
+            this.btnBaihoc3.BackColor = System.Drawing.Color.Silver;
+            this.btnBaihoc3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBaihoc3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBaihoc3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBaihoc3.Location = new System.Drawing.Point(739, 114);
+            this.btnBaihoc3.Name = "btnBaihoc3";
+            this.btnBaihoc3.Size = new System.Drawing.Size(260, 180);
+            this.btnBaihoc3.TabIndex = 7;
+            this.btnBaihoc3.UseVisualStyleBackColor = false;
+            this.btnBaihoc3.Click += new System.EventHandler(this.btnBaihoc3_Click);
+            // 
+            // btnBaihoc2
+            // 
+            this.btnBaihoc2.BackColor = System.Drawing.Color.Silver;
+            this.btnBaihoc2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBaihoc2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBaihoc2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBaihoc2.Location = new System.Drawing.Point(403, 114);
+            this.btnBaihoc2.Name = "btnBaihoc2";
+            this.btnBaihoc2.Size = new System.Drawing.Size(260, 180);
+            this.btnBaihoc2.TabIndex = 6;
+            this.btnBaihoc2.UseVisualStyleBackColor = false;
+            this.btnBaihoc2.Click += new System.EventHandler(this.btnBaihoc2_Click);
+            // 
+            // btnBaihoc1
+            // 
+            this.btnBaihoc1.BackColor = System.Drawing.Color.Silver;
+            this.btnBaihoc1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBaihoc1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBaihoc1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBaihoc1.Location = new System.Drawing.Point(69, 114);
+            this.btnBaihoc1.Name = "btnBaihoc1";
+            this.btnBaihoc1.Size = new System.Drawing.Size(260, 180);
+            this.btnBaihoc1.TabIndex = 5;
+            this.btnBaihoc1.UseVisualStyleBackColor = false;
+            this.btnBaihoc1.Click += new System.EventHandler(this.btnBaihoc1_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Gray;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1404, 82);
+            this.panel4.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(569, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KHÓA HỌC CỦA BẠN";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Controls.Add(this.dgvKetquabailam);
+            this.panel2.Controls.Add(this.panel10);
+            this.panel2.Location = new System.Drawing.Point(509, 444);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1404, 614);
+            this.panel2.TabIndex = 0;
+            // 
+            // dgvKetquabailam
+            // 
+            this.dgvKetquabailam.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvKetquabailam.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKetquabailam.Location = new System.Drawing.Point(-1, 81);
+            this.dgvKetquabailam.Name = "dgvKetquabailam";
+            this.dgvKetquabailam.RowHeadersWidth = 51;
+            this.dgvKetquabailam.RowTemplate.Height = 24;
+            this.dgvKetquabailam.Size = new System.Drawing.Size(1405, 533);
+            this.dgvKetquabailam.TabIndex = 3;
+            this.dgvKetquabailam.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvKetquabailam_CellContentClick);
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.Gray;
+            this.panel10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel10.Controls.Add(this.label2);
+            this.panel10.Location = new System.Drawing.Point(-1, -1);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(1406, 82);
+            this.panel10.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(585, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "KẾT QUẢ LÀM BÀI";
+            // 
+            // DanhSachKetQua
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Name = "DanhSachKetQua";
+            this.Text = "DanhSachKetQua";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKetquabailam)).EndInit();
+            this.panel10.ResumeLayout(false);
+            this.panel10.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Button btnKhoaHoc;
+        private System.Windows.Forms.Button btnHiennguoidung;
+        private System.Windows.Forms.DataGridView dgvKetquabailam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBaihoc1;
+        private System.Windows.Forms.Button btnBaihoc4;
+        private System.Windows.Forms.Button btnBaihoc3;
+        private System.Windows.Forms.Button btnBaihoc2;
+    }
+}
