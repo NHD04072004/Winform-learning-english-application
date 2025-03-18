b53877ffa9bba5a2d6f694eeb27f7fed1857741f
Ninh Tran Thuy <tranthuyninh.main@gmail.com>
2025-03-18 17:33:27 +0700

    Ninh commit Question

---
+++ HocTiengAnh/Adapter/QuestionAdapter.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using HocTiengAnh.Model;
+
+namespace HocTiengAnh
+{
+    class QuestionAdapter
+    {
+        string cnnStr = ConfigurationManager.ConnectionStrings["db_hoc_tieng_anh"].ConnectionString;
+        string MaKetQua_AutoGenerate = null;
+        public List<Model.Question> LoadQuestion(string MaBaiHoc)
+        {
+            List<Model.Question> questionList = new List<Model.Question>();
+            Dictionary<string, Model.Question> questionMap = new Dictionary<string, Model.Question>();
+
+            using (SqlConnection cnn = new SqlConnection(cnnStr))
+            {
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand("pr_loadQuestion", cnn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@MaBaiHoc", MaBaiHoc);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string QuestionID = reader["sMaCauHoi"].ToString();
+
+                            if (!questionMap.ContainsKey(QuestionID))
+                            {
+                                questionMap[QuestionID] = new Model.Question
+                                {
+                                    MaCauHoi = reader["sMaCauHoi"].ToString(),
+                                    MaBaiHoc = reader["sMaBaiHoc"].ToString(),
+                                    NoiDungCauHoi = reader["sNoiDungCauHoi"].ToString(),
+                                    listAnswer = new List<Answer>()
+                                };
+                            }
+
+                            Answer answer = new Answer
+                            {
+                                MaCauHoi = reader["sMaCauHoi"].ToString(),
+                                MaDapAn = reader["sMaDapAn"].ToString(),
+                                NoiDungDapAn = reader["sNoiDungDapAn"].ToString(),
+                                DapAnDung = (bool)reader["bDapAnDung"]
+                            };
+
+                            questionMap[QuestionID].listAnswer.Add(answer);
+                            
+                        }
+                    }
+                }
+            }   
+
+            questionList.AddRange(questionMap.Values);
+            return questionList;
+        }
+        public string autoGenerateMaKetQua()
+        {
+            using (SqlConnection cnn = new SqlConnection(cnnStr))
+            {
+                cnn.Open();
+                string cnnStr = "SELECT TOP 1 sMaKetQua FROM tblBangKetQua order by sMaKetQua desc";
+                using (SqlCommand cmd = new SqlCommand(cnnStr, cnn))
+                {
+                    var result = cmd.ExecuteScalar();
+                    if(result != null)
+                    {
+
+                        return "KQ" + (int.Parse(result.ToString().Substring(2)) + 1).ToString();
+                    }
+                    else
+                    {
+                        return "KQ001";
+                    }
+                }
+            }
+        }
+        public string autoGenerateMaTraLoi()
+        {
+            using (SqlConnection cnn = new SqlConnection(cnnStr))
+            {
+                cnn.Open();
+                string query = "SELECT TOP 1 sMaTraLoi FROM tblBangTraLoi ORDER BY sMaTraLoi DESC";
+                using (SqlCommand cmd = new SqlCommand(query, cnn))
+                {
+                    var result = cmd.ExecuteScalar();
+                    if (result != null)
+                    {
+                        int newId = int.Parse(result.ToString().Substring(2)) + 1;
+                        return "TL" + newId.ToString("D3"); // D3 đảm bảo mã có dạng TL001, TL002, ...
+                    }
+                    else
+                    {
+                        return "TL001";
+                    }
+                }
+            }
+        }
+
+        public void CreateResult(string MaTaiKhoan, string MaBaiHoc)
+        {
+            using (SqlConnection cnn = new SqlConnection(cnnStr))
+            {
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand("pr_createResult", cnn))
+                {
+                    MaKetQua_AutoGenerate = autoGenerateMaKetQua();
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@MaKetQua", MaKetQua_AutoGenerate);
+                    cmd.Parameters.Add("@MaTaiKhoan", MaTaiKhoan);
+                    cmd.Parameters.Add("@MaBaiHoc", MaBaiHoc);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public void CreateResultDetail(string MaKetQua, string MaCauHoi, string MaDapAn, bool DapAnDung)
+        {
+            using (SqlConnection cnn = new SqlConnection(cnnStr))
+            {
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand("pr_createResultDetail", cnn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@MaKetQua", MaKetQua_AutoGenerate);
+                    cmd.Parameters.Add("@MaTraLoi", autoGenerateMaTraLoi());
+                    cmd.Parameters.Add("@MaCauHoi", MaCauHoi);
+                    cmd.Parameters.Add("@MaDapAnTraLoi", MaDapAn);
+                    cmd.Parameters.Add("@DapAnDung", DapAnDung);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public void FinishTest(string MaKetQua, DateTime ThoiGianNop)
+        {
+            using (SqlConnection cnn = new SqlConnection(cnnStr))
+            {
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand("pr_finishTest", cnn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@MaKetQua", MaKetQua);
+                    cmd.Parameters.Add("@ThoiGianNop", ThoiGianNop);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ HocTiengAnh/Question.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace HocTiengAnh
+{
+    partial class Question
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.lblQuestionContent = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblQuestion.Location = new System.Drawing.Point(3, 0);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(792, 83);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "Question";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblQuestionContent
+            // 
+            this.lblQuestionContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblQuestionContent.AutoSize = true;
+            this.lblQuestionContent.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblQuestionContent.Location = new System.Drawing.Point(3, 83);
+            this.lblQuestionContent.Name = "lblQuestionContent";
+            this.lblQuestionContent.Size = new System.Drawing.Size(792, 83);
+            this.lblQuestionContent.TabIndex = 1;
+            this.lblQuestionContent.Text = "Question Content";
+            this.lblQuestionContent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button4, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(-2, 171);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(804, 282);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.AutoSize = true;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button4.Location = new System.Drawing.Point(405, 144);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(396, 135);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnAnswer_onClick);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.AutoSize = true;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button3.Location = new System.Drawing.Point(3, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(396, 135);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnAnswer_onClick);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button2.Location = new System.Drawing.Point(405, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(396, 135);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnAnswer_onClick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(396, 135);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnAnswer_onClick);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSubmit.Location = new System.Drawing.Point(0, 480);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(800, 48);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Nộp";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_onClick);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.lblQuestionContent, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.lblQuestion, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(1, 2);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(798, 166);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNext.Location = new System.Drawing.Point(1, 480);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(801, 48);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // Question
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 528);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Name = "Question";
+            this.Text = "Question";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label lblQuestionContent;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
