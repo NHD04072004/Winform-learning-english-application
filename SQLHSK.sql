ebec1d0a6c4bb78be97522e1e9d17c8b356eb242
ninhnin <117137384+NinhTranThuy@users.noreply.github.com>
2025-03-19 13:16:29 +0700

    Update SQLHSK.sql

--- SQLHSK.sql
+++ SQLHSK.sql
@@ -524,3 +524,104 @@ END
 exec dbo.DSCau_hoi @mabaihoc = 'BH001';
 
 drop proc DSCau_hoi*/
+
+
+--TRIGGER AND PROC NINH
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TRIGGER [dbo].[tg_sumScore]
+ON [dbo].[tblBangTraLoi]
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    PRINT 'Trigger tg_sumScore đã chạy!';
+    
+    UPDATE bk
+    SET fDiem = ISNULL((
+        SELECT COUNT(tl.sMaTraLoi) * 2
+        FROM tblBangTraLoi tl
+        WHERE tl.sMaKetQua = bk.sMaKetQua
+        AND tl.bDapAnDung = 1
+    ), 0)  -- Nếu kết quả NULL, đặt về 0
+    FROM tblBangKetQua bk
+    JOIN inserted i ON bk.sMaKetQua = i.sMaKetQua;
+END;
+GO
+ALTER TABLE [dbo].[tblBangTraLoi] ENABLE TRIGGER [tg_sumScore]
+GO
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create proc [dbo].[pr_createResult]
+@MaKetQua varchar(10),
+@MaTaiKhoan int,
+@MaBaiHoc varchar(10)
+as
+	insert into tblBangKetQua(sMaKetQua,iMaTK,sMaBaiHoc)
+	values (@MaKetQua, @MaTaiKhoan, @MaBaiHoc)
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE proc [dbo].[pr_createResultDetail]
+@MaKetQua varchar(10),
+@MaTraLoi varchar(10),
+@MaCauHoi varchar(10),
+@MaDapAnTraLoi varchar(10),
+@DapAnDung bit
+as
+	insert into tblBangTraLoi(sMaTraLoi,sMaKetQua,sMaCauHoi,sMaDapAnTraLoi,bDapAnDung)
+	values (@MaTraLoi,@MaKetQua,@MaCauHoi,@MaDapAnTraLoi,@DapAnDung)
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create proc [dbo].[pr_finishTest]
+@MaKetQua VARCHAR(10),
+@ThoiGianNop DATETIME
+as
+	UPDATE tblBangKetQua
+    SET dThoiGianNop = @ThoiGianNop
+    WHERE sMaKetQua = @MaKetQua;
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create proc [dbo].[pr_getResult]
+@MaTK int,
+@MaBaiHoc VARCHAR(10)
+AS
+	select * from tblBangKetQua as kq
+	where kq.iMaTK = @MaTK and kq.sMaBaiHoc LIKE @MaBaiHoc
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create proc [dbo].[pr_loadQuestion]
+@MaBaiHoc VARCHAR(10)
+as
+begin
+	SELECT * 
+	FROM tblCauHoi
+	JOIN tblDapAn ON tblCauHoi.sMaCauHoi = tblDapAn.sMaCauHoi
+	WHERE tblCauHoi.sMaBaiHoc LIKE @MaBaiHoc
+	ORDER BY tblCauHoi.sMaCauHoi ASC;
+end
+GO
